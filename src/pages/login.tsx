7542631d494f95ee75e8e2ca4bc708207558f15e
Liioan <dev.davidbijok@gmail.com>
2023-07-21 16:40:25 +0200

    add login page

---
+++ src/pages/login.tsx
@@ -0,0 +1,47 @@
+import { type GetServerSideProps, type NextPage } from "next";
+import { signIn } from "next-auth/react";
+import Image from "next/image";
+import { getServerAuthSession } from "~/server/auth";
+
+const Login: NextPage = () => {
+  return (
+    <main className="flex h-screen items-center justify-center bg-background-light dark:bg-background-dark">
+      <div className="flex w-mobile flex-col items-center gap-10 rounded-15 bg-foreground-light py-10 dark:bg-foreground-dark">
+        <Image
+          src={"/Logo_with_icon_dark.png"}
+          width={150}
+          height={78}
+          alt="logo"
+        />
+        <p className="text-center text-big">Note app</p>
+        <button
+          className="rounded-5 bg-primary-light px-4 py-2 font-bold text-text-light dark:bg-primary-dark dark:text-background-dark"
+          onClick={() => void signIn("discord")}
+        >
+          Sign in with discord
+        </button>
+      </div>
+    </main>
+  );
+};
+
+export default Login;
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const session = await getServerAuthSession(ctx);
+
+  if (session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      session,
+    },
+  };
+};
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,54 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        mobile: "338px",
+      },
+      borderRadius: {
+        15: "15px",
+        5: "5px",
+      },
+      fontSize: {
+        small: "0.938",
+        big: "1.25rem",
+      },
+    },
+    colors: {
+      background: {
+        dark: "#16161E",
+        light: "#CBCCD1",
+      },
+      foreground: {
+        dark: "#1E2130",
+        light: "#D5D6DB",
+      },
+      text: {
+        dark: "#F5F5F5",
+        light: "#0F0F14",
+      },
+      primary: {
+        dark: "#44CBCA",
+        light: "#148DA1",
+      },
+      secondary: {
+        dark: "#7AA2F7",
+        light: "#375FA0",
+      },
+      accent: {
+        dark: "#A67AFF",
+        light: "#6B588D",
+      },
+      danger: {
+        dark: "#F7768E",
+        light: "#A62941",
+      },
+      accept: {
+        dark: "#9ECE6A",
+        light: "#5B921F",
+      },
+    },
   },
   plugins: [],
+  darkMode: "class",
 } satisfies Config;
