a8544281ff0c6c3b80cb526546e002e81b765aa3
Liioan <dev.davidbijok@gmail.com>
2023-07-27 19:43:49 +0200

    bugfix

--- src/components/ui/DeleteButton.tsx
+++ src/components/ui/DeleteButton.tsx
@@ -2,17 +2,20 @@ import { api } from "~/utils/api";
 import Icon from "../global/Icon";
 import { useUiStore } from "~/store/useUiStore";
 import usePopup from "~/hooks/usePopup";
+import { useRouter } from "next/router";
 
 interface DeleteButtonProps {
   id: string;
 }
 
 const DeleteButton = ({ id }: DeleteButtonProps) => {
+  const router = useRouter();
   const { openPopup } = usePopup();
 
   const deleteNote = api.note.deleteNote.useMutation({
     onSuccess(message) {
       openPopup(message, false);
+      router.back();
     },
     onError(data) {
       openPopup(data.message, true);
@@ -22,7 +25,9 @@ const DeleteButton = ({ id }: DeleteButtonProps) => {
   return (
     <button
       className="flex h-8 w-8 items-center justify-center rounded-full bg-danger-light dark:bg-danger-dark"
-      onClick={() => deleteNote.mutate({ noteId: id })}
+      onClick={() => {
+        deleteNote.mutate({ noteId: id });
+      }}
     >
       <Icon iconName="delete" className="text-text-light" />
     </button>
