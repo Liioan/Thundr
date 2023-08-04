f90cf4ab4fc6850435ca9a0a1a822af2b4c65223
Liioan <dev.davidbijok@gmail.com>
2023-08-04 14:56:51 +0200

    fix bg transition in title

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -22,10 +22,8 @@ const Title = ({
   return (
     <div className="after: relative after:absolute after:-bottom-[2px] after:left-0 after:h-[4px] after:w-full after:rounded-full after:bg-secondary-light after:content-['']  dark:after:bg-secondary-dark ">
       <TextArea
-        className={`mb-2 resize-none text-medium font-medium text-secondary-light focus:text-primary-light focus:outline-none  dark:text-secondary-dark dark:focus:text-primary-dark ${
-          isDisabled
-            ? "max-h-8 w-full overflow-hidden bg-foreground-light dark:bg-foreground-dark "
-            : " w-full bg-background-light dark:bg-background-dark"
+        className={`mb-2 resize-none bg-transparent text-medium font-medium text-secondary-light focus:text-primary-light focus:outline-none  dark:text-secondary-dark dark:focus:text-primary-dark ${
+          isDisabled ? "max-h-8 w-full overflow-hidden " : " w-full"
         }`}
         text={insertText()}
         isDisabled={isDisabled}
